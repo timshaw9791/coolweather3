bc50b9f43655cdeefbc4162e44b03cddefcba4a0
admin <abc@qq.com>
2019-04-24 09:27:43 +0800

    省市县三级联动

---
+++ app/src/main/java/com/example/admin/myapplication/Abc.java
@@ -0,0 +1,54 @@
+package com.example.admin.myapplication;
+
+public class Abc {
+    public static int count=0;//经过车辆数
+    public static void main(String args[]){
+
+
+        (new Thread(){
+            @Override
+            public void run() {
+               System.out.println("hello world1");
+               while(true){
+                   try {
+                       Thread.sleep(1000);
+                   } catch (InterruptedException e) {
+                       e.printStackTrace();
+                   }
+                    countCar();
+               }
+            }
+        }).start();
+
+        (new Thread(){
+            @Override
+            public void run() {
+                while(true){
+
+                    printCar();
+                  //  if(count!=0){
+
+                   // }
+
+                }
+            }
+        }).start();
+    }
+
+    synchronized static void  printCar(){
+        if(count!=0){
+        System.out.println("过去的车辆数=>"+count);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        count=0;
+        }
+    }
+
+    synchronized   static void countCar(){
+        count++;
+        System.out.println("又过了1辆车");
+    }
+}
--- app/src/main/java/com/example/admin/myapplication/CountyActivity.java
+++ app/src/main/java/com/example/admin/myapplication/CountyActivity.java
@@ -3,8 +3,6 @@ package com.example.admin.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -13,31 +11,24 @@ import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
-
-
-    private TextView textView;
-    private Button button;
-
+public class CountyActivity extends AppCompatActivity {
+    private TextView textView=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        this.textView = (TextView) findViewById(R.id.abc);
-        this.button=(Button)findViewById(R.id.btn);
-        this.button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,Main2Activity.class));
-            }
-        });
-        String weatherId = "CN101230201";
-        String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=f691985632d949abb8df27de3516d2f3";
-        HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
+        setContentView(R.layout.activity_county);
+        this.textView=findViewById(R.id.textView);
+
+        Intent intent=getIntent();
+        int cityid=intent.getIntExtra("cid",0);
+        int pid=intent.getIntExtra("pid",0);
+
+        String url="http://guolin.tech/api/china/"+pid+"/"+cityid;
+        HttpUtil.sendOkHttpRequest(url, new Callback() {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 final String responseText = response.body().string();
-
+               // parseJSONObject(responseText);
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
